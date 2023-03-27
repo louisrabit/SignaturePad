063cbba6e0fab3a42ee5213910455b8c6e916a3e
luis coelho <luiscoelho18994@gmail.com>
2023-03-27 22:15:32 +0100

    touch5

--- canvas.js
+++ canvas.js
@@ -49,26 +49,54 @@ canvas.addEventListener('mouseup', () => {
 ------------------------------------------- */
 
 
-
-canvas.addEventListener('touchstart', (event) => {
-isDrawing = true;
-[lastX, lastY] = [event.clientX, event.clientY];
+// Set up touch events for mobile, etc
+canvas.addEventListener("touchstart", function (e) {
+  mousePos = getTouchPos(canvas, e);
+var touch = e.touches[0];
+var mouseEvent = new MouseEvent("mousedown", {
+clientX: touch.clientX,
+clientY: touch.clientY
 });
-canvas.addEventListener('touchmove', (event) => {
-if (!isDrawing) return;
-ctx.beginPath();
-ctx.moveTo(lastX, lastY);''
-ctx.lineTo(event.clientX,  event.clientY);
-ctx.lineWidth = 3;
-ctx.stroke();
-[lastX, lastY] = [event.clientX, event.clientY];
+canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchend", function (e) {
+var mouseEvent = new MouseEvent("mouseup", {});
+canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchmove", function (e) {
+var touch = e.touches[0];
+var mouseEvent = new MouseEvent("mousemove", {
+clientX: touch.clientX,
+clientY: touch.clientY
 });
-canvas.addEventListener('touchend', () => {
-isDrawing = false;
-});
+canvas.dispatchEvent(mouseEvent);
+}, false);
 
+// Get the position of a touch relative to the canvas
+function getTouchPos(canvasDom, touchEvent) {
+var rect = canvasDom.getBoundingClientRect();
+return {
+x: touchEvent.touches[0].clientX - rect.left,
+y: touchEvent.touches[0].clientY - rect.top
+};
+}
 
-
+// Prevent scrolling when touching the canvas
+document.body.addEventListener("touchstart", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchend", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchmove", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
 
 
 //clear button
