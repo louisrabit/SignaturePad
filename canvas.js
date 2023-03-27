bb7724a5b0f13e4a1194d28a3185da5c51f291c5
luis coelho <luiscoelho18994@gmail.com>
2023-03-27 21:47:27 +0100

    touchscreen

--- canvas.js
+++ canvas.js
@@ -47,26 +47,6 @@ canvas.addEventListener('mouseup', () => {
 });
 
 
-//clear button
-document.getElementById('clear').addEventListener('click', function() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  });
-
-
-
-
-
-      //save png file 
-      const download = document.getElementById('download');
-      download.addEventListener('click', function (e) {
-        const link = document.createElement('a');
-        link.download = 'download.png';
-        link.href = canvas.toDataURL();
-        link.click();
-        link.delete;
-      });
-
-
 
       // phone touch screen 
 
@@ -81,3 +61,70 @@ document.getElementById('clear').addEventListener('click', function() {
         var y = event.touches[0].clientY-canvas.offsetLeft;
         isDrawing(x,y);
       }) */
+
+      canvas.addEventListener('touchstart', (event) => {
+        //defines variable as true 
+      isDrawing = true;
+      //store values inicial properties "store" of event in the variables latX/lastY
+      [lastX, lastY] = [event.clientX, event.clientY];
+    });
+    
+    
+    //when user move mouse
+    //check first if isdrawing is true , if not function return , and dont do nothing 
+    canvas.addEventListener('touchmove', (event) => {
+      if (!isDrawing) return;
+    
+      //if function is true , call beginPath of redering context of ctx
+      ctx.beginPath();
+      //first values stores in variables lastX, lastY
+      ctx.moveTo(lastX, lastY);
+    
+      // event property of js that have coordinaties X/y that occur the event related with elemenet that receive that event .
+      //not supported with previous editions of browser so we use CLIENTX/CLIENTY
+    /*   ctx.lineTo(event.offsetX,  event.offsetY); */
+    
+    //final mouse coordinate 
+    ctx.lineTo(event.clientX,  event.clientY);
+    
+    // Bold line (change line ) == thickness
+    ctx.lineWidth = 3;
+    //draw the line 
+      ctx.stroke();
+      //update the values with final coordinates of the final point of the drawing 
+      [lastX, lastY] = [event.clientX, event.clientY];
+    });
+    
+    canvas.addEventListener('touchend', () => {
+      isDrawing = false;
+    });
+    
+    
+    
+    
+    
+    
+//clear button
+document.getElementById('clear').addEventListener('click', function() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+
+
+
+
+    //save png file 
+    const download = document.getElementById('download');
+    download.addEventListener('click', function (e) {
+      const link = document.createElement('a');
+      link.download = 'download.png';
+      link.href = canvas.toDataURL();
+      link.click();
+      link.delete;
+    });
+
+
+    
+    
+       
+    
