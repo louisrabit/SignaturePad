6ef4b4c539c7d0eecb487a6eba571695f0f4d643
luis coelho <luiscoelho18994@gmail.com>
2023-03-28 22:59:01 +0100

    canvas position/ responsive + reajust X && Y

--- canvas.js
+++ canvas.js
@@ -7,102 +7,75 @@ let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 
-
-// click/press on the button
 canvas.addEventListener('mousedown', (event) => {
-    //defines variable as true 
   isDrawing = true;
-  //store values inicial properties "store" of event in the variables latX/lastY
-  [lastX, lastY] = [event.clientX, event.clientY];
+  // Use getBoundingClientRect() to get the position of the canvas relative to the viewport
+  const rect = canvas.getBoundingClientRect();
+  // Store the mouse position relative to the canvas in lastX and lastY
+  lastX = event.clientX - rect.left;
+  lastY = event.clientY - rect.top;
 });
 
-
-//when user move mouse
-//check first if isdrawing is true , if not function return , and dont do nothing 
 canvas.addEventListener('mousemove', (event) => {
   if (!isDrawing) return;
 
-  //if function is true , call beginPath of redering context of ctx
   ctx.beginPath();
-  //first values stores in variables lastX, lastY
-  ctx.moveTo(lastX, lastY);''
-
-  // event property of js that have coordinaties X/y that occur the event related with elemenet that receive that event .
-  //not supported with previous editions of browser so we use CLIENTX/CLIENTY
-/*   ctx.lineTo(event.offsetX,  event.offsetY); */
-
-//final mouse coordinate 
-ctx.lineTo(event.clientX,  event.clientY);
-
-// Bold line (change line ) == thickness
-ctx.lineWidth = 3;
-//draw the line 
+  ctx.moveTo(lastX, lastY);
+  // Use getBoundingClientRect() to get the position of the canvas relative to the viewport
+  const rect = canvas.getBoundingClientRect();
+  // Update lastX and lastY with the new mouse position relative to the canvas
+  lastX = event.clientX - rect.left;
+  lastY = event.clientY - rect.top;
+  ctx.lineTo(lastX, lastY);
+  ctx.lineWidth = 3;
   ctx.stroke();
-  //update the values with final coordinates of the final point of the drawing 
-  [lastX, lastY] = [event.clientX, event.clientY];
 });
 
 canvas.addEventListener('mouseup', () => {
   isDrawing = false;
 });
-/* ..........................................
-------------------------------------------- */
-
 
 // Set up touch events for mobile, etc
 canvas.addEventListener("touchstart", function (e) {
-  mousePos = getTouchPos(canvas, e);
-var touch = e.touches[0];
-var mouseEvent = new MouseEvent("mousedown", {
-clientX: touch.clientX,
-clientY: touch.clientY
-});
-canvas.dispatchEvent(mouseEvent);
-}, false);
-canvas.addEventListener("touchend", function (e) {
-var mouseEvent = new MouseEvent("mouseup", {});
-canvas.dispatchEvent(mouseEvent);
-}, false);
-canvas.addEventListener("touchmove", function (e) {
-var touch = e.touches[0];
-var mouseEvent = new MouseEvent("mousemove", {
-clientX: touch.clientX,
-clientY: touch.clientY
-});
-canvas.dispatchEvent(mouseEvent);
-}, false);
-
-// Get the position of a touch relative to the canvas
-function getTouchPos(canvasDom, touchEvent) {
-var rect = canvasDom.getBoundingClientRect();
-return {
-x: touchEvent.touches[0].clientX - rect.left,
-y: touchEvent.touches[0].clientY - rect.top
-};
-}
-
-
-
-
-//clear button
-document.getElementById('clear').addEventListener('click', function() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const touch = e.touches[0];
+  // Create a new mouse event based on the touch event
+  const mouseEvent = new MouseEvent("mousedown", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
   });
+  // Dispatch the mouse event on the canvas
+  canvas.dispatchEvent(mouseEvent);
+}, false);
 
+canvas.addEventListener("touchend", function (e) {
+  const mouseEvent = new MouseEvent("mouseup", {});
+  canvas.dispatchEvent(mouseEvent);
+}, false);
 
+canvas.addEventListener("touchmove", function (e) {
+  const touch = e.touches[0];
+  const mouseEvent = new MouseEvent("mousemove", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
 
+// Clear button
+document.getElementById('clear').addEventListener('click', function() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
 
-
-      //save png file 
-      const download = document.getElementById('download');
-      download.addEventListener('click', function (e) {
-        const link = document.createElement('a');
-        link.download = 'download.png';
-        link.href = canvas.toDataURL();
-        link.click();
-        link.delete;
-      });
-
-
-
-     
+// Download button
+const download = document.getElementById('download');
+download.addEventListener('click', function (e) {
+  // Create a new anchor element
+  const link = document.createElement('a');
+  link.download = 'download.png';
+  // Set the href of the anchor element to the data URL of the canvas image
+  link.href = canvas.toDataURL();
+  // Dispatch a click event on the anchor element to trigger the download
+  link.click();
+  // Remove the anchor element from the DOM
+  link.remove();
+});
